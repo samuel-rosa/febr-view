235e1eceeeccce5f20985cd8a3782b7a502fefb4
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-11-29 20:55:10 -0200

    ignore data; code cleaning

--- code/helper.R
+++ code/helper.R
@@ -9,24 +9,19 @@ createSiteMetadata <-
     sharing <- paste(drive_folder, sharing, sep = "")
     
     # Definir opções de local
-    locale <- readr::locale(decimal_mark = ",")
+    locale <- locale(decimal_mark = ",")
+    na <- c("NA", "-", "")
     
     # Descarregar dados
     dataset <- suppressMessages(
-      googlesheets::gs_read_csv(
-        googlesheets::gs_key(dataset), verbose = FALSE, na = c("NA", "-", ""), locale = locale))
+      gs_read_csv(gs_key(dataset), verbose = FALSE, na = na, locale = locale))
     obs_cols <- c("observacao_id", "estado_id", "coord_sistema", "coord_x", "coord_y")
     observation <- suppressMessages(
-      googlesheets::gs_read_csv(
-        googlesheets::gs_key(observation), verbose = FALSE, na = c("NA", "-", ""), 
-        locale = locale)[, obs_cols])
+      gs_read_csv(s_key(observation), verbose = FALSE, na = na, locale = locale)[, obs_cols])
     layer <- suppressMessages(
-      googlesheets::gs_read_csv(
-        googlesheets::gs_key(layer), comment = "unidade", verbose = FALSE, na = c("NA", "-", ""), 
-        locale = locale))
+      gs_read_csv(gs_key(layer), comment = "unidade", verbose = FALSE, na = na, locale = locale))
     
-    # Processar dados das coordenadas espaciais -- independente de possuir ou não dados de ferro -- com 
-    # transformação do sistema de referência de coordenadas para EPSG:4326 (se necessário)
+    # Processar dados das coordenadas espaciais com transformação do SRC para EPSG:4326 (se necessário)
     obs_coords <- na.exclude(observation)
     if (nrow(obs_coords) >= 1) {
       if (nlevels(as.factor(obs_coords$coord_sistema)) > 1) {
@@ -37,43 +32,39 @@ createSiteMetadata <-
           j <- 1:length(obs_coords)
         }
         obs_coords[j] <- lapply(obs_coords[j], function (x) {
-          sp::coordinates(x) <- c("coord_x", "coord_y")
-          sp::proj4string(x) <- sp::CRS(paste("+init=", tolower(x$coord_sistema[1]), sep = ""))
-          x <- sp::spTransform(x, sp::CRS("+init=epsg:4326"))
-          as.data.frame(x)
+          coordinates(x) <- c("coord_x", "coord_y")
+          proj4string(x) <- CRS(paste("+init=", tolower(x$coord_sistema[1]), sep = ""))
+          x <- spTransform(x, CRS("+init=epsg:4326")) %>% as.data.frame()
         })
         obs_coords <- do.call(rbind, obs_coords)
         
       } else if (unique(obs_coords$coord_sistema) != "EPSG:4326") {
-        sp::coordinates(obs_coords) <- c("coord_x", "coord_y")
-        sp::proj4string(obs_coords) <- 
-          sp::CRS(paste("+init=", tolower(obs_coords$coord_sistema[1]), sep = ""))
-        obs_coords <- sp::spTransform(obs_coords, sp::CRS("+init=epsg:4326"))
-        obs_coords <- as.data.frame(obs_coords)
+        coordinates(obs_coords) <- c("coord_x", "coord_y")
+        proj4string(obs_coords) <- CRS(paste("+init=", tolower(obs_coords$coord_sistema[1]), sep = ""))
+        obs_coords <- spTransform(obs_coords, CRS("+init=epsg:4326")) %>% as.data.frame()
       }
-      obs_coords$observacao_id <- paste(obs_coords$observacao_id, "@", n, sep = "")
-      obs_coords$sharing <- paste('<a href="', sharing, '">Acessar dados</a>', sep = "")
-      obs_coords$mailto <- paste(
-        '<a href="mailto:febr-forum@googlegroups.com?subject=', obs_coords$observacao_id, 
-        '">Relatar problema</a>', sep = "")
+      obs_coords$observacao_id <- glue("{obs_coords$observacao_id}@{n}")
+      obs_coords$sharing <- glue('<a href="{sharing}">Acessar dados</a>')
+      obs_coords$mailto <- glue(
+        '<a href="mailto:febr-forum@googlegroups.com?subject={obs_coords$observacao_id}">Relatar problema</a>')
       rownames(obs_coords) <- NULL
       
       # Salvar arquivo com as coordenadas das observações para publicação no website
       # file <- paste("./febr-website/data/", n, "-coords.csv", sep = "")
       file <- glue('../data/{n}-coords.csv')
-      write.csv(
-        obs_coords[, c("observacao_id", "coord_x", "coord_y", "sharing", "mailto")], 
-        file = file, fileEncoding = "UTF-8") 
+      obs_coords %>% 
+        select(observacao_id, coord_x, coord_y, sharing, mailto) %>%
+        write.csv(file = file, fileEncoding = "UTF-8") 
       
     }
     
     # Agregar dados das observações e camadas
-    # Usar apenas as colunas necessárias: assume-se que o número máximo de colunas necessárias da tabela
-    # 'layer' seja 15.
-    db <- merge(
-      observation[, c("observacao_id", "estado_id")],
-      layer[, 1:ifelse(ncol(layer) > 15, 15, ncol(layer))],
-      by = "observacao_id")
+    # Usar apenas as colunas necessárias da tabela 'layer', ou seja, 5.
+    # db <- merge(
+      # observation[, c("observacao_id", "estado_id")],
+      # layer[, 1:ifelse(ncol(layer) > 15, 15, ncol(layer))],
+      # by = "observacao_id")
+    db <- merge(observation[, c("observacao_id", "estado_id")], layer[, 1:5], by = "observacao_id")
     
     # Identificar linhas e colunas contendo dados de ferro
     # Gerar metadados apenas se realmente houver dados de ferro
@@ -92,7 +83,7 @@ createSiteMetadata <-
       
       # Preparar nome dos autores. Para conjuntos de dados com múltiplos autores, apenas os dois primeiros
       # são apresentados.
-      Nome <- stringr::str_split_fixed(dataset[dataset$item == "autor_nome", 2], ";", n = Inf)
+      Nome <- str_split_fixed(dataset[dataset$item == "autor_nome", 2], ";", n = Inf)
       if (length(Nome) > 2) {
         Nome <- paste(paste(Nome[1:2], collapse = "; "), "et alli")
       } #else {
@@ -102,8 +93,7 @@ createSiteMetadata <-
       # Preparar descrição da contribuição 
       ctb <- data.frame(
         Nome = paste(Nome, collapse = "; "),
-        Instituição =
-          stringr::str_split_fixed(dataset[dataset$item == "organizacao_nome", 2], ";", n = Inf)[1],
+        Instituição = str_split_fixed(dataset[dataset$item == "organizacao_nome", 2], ";", n = Inf)[1],
         Título = dataset[[2]][dataset$item == "dataset_titulo"],
         # UF = levels(as.factor(db[id_row, "estado_id"])),
         UF = levels(as.factor(db[, "estado_id"])),
