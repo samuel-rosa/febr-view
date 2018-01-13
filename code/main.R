215c99784eaae18269284e20eb927f0eedc8573a
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-01-13 18:35:14 -0200

    novo arquivo principal

---
+++ code/main.R
@@ -0,0 +1,11 @@
+# Processar arquivo Rmd e gerar saída em HTML
+rmarkdown::render('view.Rmd', encoding = 'UTF-8', output_dir = "../docs")
+
+# Editar a linha 92 do arquivo HTML, substituindo
+file <- '../docs/view.html'
+html <- readLines(con = file)
+for (i in seq(length(html))) {
+  html[i] <- 
+    gsub(pattern = '940px', replacement = '100%', x = html[i], fixed = TRUE)
+}
+writeLines(text = html, con = file)
